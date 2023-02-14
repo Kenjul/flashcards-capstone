43cda5e8e3f8cb8a535d4983ec441df9d48925bc
Kenjul <kenjul.biz@gmail.com>
2023-02-14 12:27:32 -0800

    fixed unneeded code in study

--- src/Deck/Study.js
+++ src/Deck/Study.js
@@ -1,32 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useHistory, Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { readDeck } from "../utils/api/index";
 
 export default function Study() {
-  let history = useHistory();
   const { deckId } = useParams();
   const [deck, setDeck] = useState({ cards: [] });
   const [index, setIndex] = useState(0);
   const [flip, setFlip] = useState(false);
-  const [error, setError] = useState(undefined);
-
+  
   useEffect(() => {
     const abortController = new AbortController();
     async function loadDeck() {
       try {
         const deckFromAPI = await readDeck(deckId, abortController.signal);
         setDeck(deckFromAPI);
-      }
-      catch(e){
+      } catch (e) {
         setError(e);
       }
     }
     loadDeck();
     return () => abortController.abort();
   }, [deckId]);
-  
+
   function handleFlip() {
-    setFlip(prevState => !prevState)
+    setFlip((prevState) => !prevState);
   }
 
   function handleNext() {
@@ -51,8 +48,12 @@ export default function Study() {
             <li className="breadcrumb-item">
               <Link to="/">Home</Link>
             </li>
-            <li className="breadcrumb-item" aria-current="page"><a href={`/decks/${deck.id}`}>{deck.name}</a></li>
-            <li className="breadcrumb-item-active" aria-current="page">Study</li>
+            <li className="breadcrumb-item" aria-current="page">
+              <a href={`/decks/${deck.id}`}>{deck.name}</a>
+            </li>
+            <li className="breadcrumb-item-active" aria-current="page">
+              Study
+            </li>
           </ol>
         </nav>
         <h1>Study: {deck.name}</h1>
@@ -80,21 +81,24 @@ export default function Study() {
                   </button>
                 )}
               </div>
-        </div> ) : (
-          <div>
-            <h3>Not enough cards.</h3>
-            <p>
+            </div>
+          ) : (
+            <div>
+              <h3>Not enough cards.</h3>
+              <p>
                 You need at least 3 cards to study. There are{" "}
                 {deck.cards.length} cards in this deck.
               </p>
-            <Link to={`/decks/${deck.id}/cards/new`} className="btn btn-primary">
-              <button type="button">Add Cards</button>
-            </Link>
+              <Link
+                to={`/decks/${deck.id}/cards/new`}
+                className="btn btn-primary"
+              >
+                <button type="button">Add Cards</button>
+              </Link>
+            </div>
+          )}
         </div>
-    )}
-        </div>
-        </div>
-      );
+      </div>
+    );
   }
-
 }
